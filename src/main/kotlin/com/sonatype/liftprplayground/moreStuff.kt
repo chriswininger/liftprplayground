56310fcb53c8942373d32eb7125676185a024244
chriswininger-test <85687756+chriswininger-test@users.noreply.github.com>
2022-06-29 17:27:14 -0400

    Update moreStuff.kt

--- src/main/kotlin/com/sonatype/liftprplayground/moreStuff.kt
+++ src/main/kotlin/com/sonatype/liftprplayground/moreStuff.kt
@@ -1,5 +1,10 @@
 package com.sonatype.liftprplayground
 
+
 fun grr() {
-  throw RuntimeException("Squirrel why must you taunt me")
+  throw MyException("Squirrel why must you taunt me")
+  
+  println("I am unreachable")
 }
+
+class MyException(msg: String?) : RuntimeException(msg)
